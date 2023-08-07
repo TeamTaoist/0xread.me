af4fde75717f76859ee5c15ec5f6b3c378305ed5
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-08-07 20:10:31 +0800

    serve: update

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,20 @@
-const express = require("express");
+import { config } from "dotenv";
+import express from "express";
+import groupApi from "./router/group";
+import cors from "cors";
+
 const app = express();
 const port = 3000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req: any, res: any) => {
   res.send("Hello World!");
 });
+
+app.use("/group", groupApi);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
